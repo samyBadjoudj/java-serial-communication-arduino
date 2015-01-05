75286184105cf64f50b1680d84f8abe8737696f9
Samy Badjoudj <samyBadjoudj@users.noreply.github.com>
2015-01-05 15:08:40 +0100

    Update Command.java

--- src/com/samy/serial/communication/reader/Command.java
+++ src/com/samy/serial/communication/reader/Command.java
@@ -1,7 +1,7 @@
 package com.samy.serial.communication.reader;
 
 /**
- * Created by bibi on 04.01.15.
+ * Created by Samy Badjoudj on 04.01.15.
  */
 public interface Command {
     public void execute(Integer value);
